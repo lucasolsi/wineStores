27e81101b7d28d791cd1f9c732691e83cb98f86f
Lucas Oliveira Silva <lucasolsi@gmail.com>
2021-06-06 12:37:59 -0300

    Initial commit

---
+++ src/main/java/com/java/wine/lucas/winestores/WineStoresApplication.java
@@ -0,0 +1,15 @@
+package com.java.wine.lucas.winestores;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WineStoresApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(WineStoresApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/java/wine/lucas/winestores/WineStoresApplicationTests.java
@@ -0,0 +1,15 @@
+package com.java.wine.lucas.winestores;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WineStoresApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
